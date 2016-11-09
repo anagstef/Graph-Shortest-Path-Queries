8540bb851ab77861ea91d2b5fb7ea00d426b5ad6
helena <sdi1300091@di.uoa.gr>
2016-11-09 17:17:10 +0200

    prob. fixed need a bit of checking

--- Queue.cpp
+++ Queue.cpp
@@ -5,62 +5,83 @@
 using namespace std;
 
 Queue::Queue() {
-    capacity = QUEUE_INIT_SIZE;
-    queue = (uint32_t*) malloc(sizeof(uint32_t) * capacity);
-    front = 0;
-    end = 0;
-    size = 0;
+    capacity = QUEUE_INIT_SIZE; //init capacity
+    queue = (uint32_t*) malloc(sizeof(uint32_t) * capacity); //malloc the array
+    front = 0; //set front to 0
+    end = 0;   //set end to 0
+    size = 0;  //queue is empty
 }
 
 Queue::~Queue() {
-    free(queue);
+    free(queue); //delete the array
 }
 
-bool Queue::isEmpty() {
+bool Queue::isEmpty() { //return the queue status
     if (size == 0) {
         return true;
     }
     return false;
 }
 
-bool Queue::isFull() {
-    if ((end+1)%capacity == front) {
+bool Queue::isFull() { //return the queue status
+    if ((size+1) == capacity) {
         return true;
     }
     return false;
 }
 
 void Queue::push(uint32_t value) {
-    if (isEmpty()) {
-        queue[front] = value;
-        end = (end+1)%capacity;
-        size++;
-        return;
+//    if (isEmpty()) { //if queue is empty
+//        cout << "pushing " << value << endl;
+//        queue[front] = value;   //append the element at the front of the queue
+//        end = (end+1)%capacity; //set ending node
+//        cout << queue[front] << " " << end << endl;
+//        size++; //increase the size (how many elements are in the queue)
+//        return;
+//    }
+    if (isFull()) { //if queue is full
+        increaseCapacity(); //realloc the array
     }
-    if (isFull()) {
-        increaseCapacity();
+    else {
+        cout << "No increase needed" << endl;
     }
-    queue[end] = value;
-    end = (end+1)%capacity;
-    size++;
+    queue[end] = value; //append new element at the end of the queue
+    end = (end+1)%capacity; //set ending node
+    size++; //increase the size
+    cout << "Pushed " << value << endl;
+    printQueue();
 }
 
 void Queue::increaseCapacity() {
+    cout << "MEGALWNWWWW" << endl;
+    //printQueue();
     uint32_t new_capacity = capacity*2;
-    uint32_t* new_queue = (uint32_t*) malloc(sizeof(uint32_t) * new_capacity);
+    cout << "cap " << capacity << " new cap " << new_capacity << endl;
+    queue = (uint32_t*) realloc(queue, sizeof(uint32_t) * new_capacity);
+    uint32_t j = capacity+1;
+    //cout << "--------" << queue[0] << endl;
+    for (uint32_t i = 0; i <=end; ++i) {
+        queue[j] = queue[i];
+        j++;
+    }
+    capacity = new_capacity;
+
+    /*uint32_t* new_queue = (uint32_t*) malloc(sizeof(uint32_t) * new_capacity);
     uint32_t j = 0;
+
+
     if (front == 0) {
-        for (uint32_t i = front; i < capacity; ++i) {
+        for (uint32_t i = front; i < capacity-1; ++i) {
             new_queue[j] = queue[i];
             j++;
         }
     }
     else {
-        for (uint32_t i = front; i < capacity; ++i) {
+        for (uint32_t i = front; i < capacity-1; ++i) {
             new_queue[j] = queue[i];
             j++;
         }
-        for (uint32_t i = 0; i <= end; ++i) {
+        for (uint32_t i = 0; i < end; ++i) {
             new_queue[j] = queue[i];
             j++;
         }
@@ -70,13 +91,13 @@ void Queue::increaseCapacity() {
     end = capacity;
     free(queue);
     capacity = new_capacity;
-    queue = new_queue;
+    queue = new_queue;*/
 }
 
 uint32_t Queue::pop() {
     uint32_t ret_val;
     ret_val = queue[front];
-    front = (front+1)%capacity;
+    front = front+1;
     size--;
     return ret_val;
 }
@@ -93,29 +114,30 @@ void Queue::printQueue() {
     if (front == 0) {
         cout << "1 " << front << " " << end << endl;
         for (uint32_t i = front; i < end; ++i) {
-            cout << queue[i] << endl;
+            printf("%d, ", queue[i]);
         }
     }
     else if (front < end) {
-        cout << "2" << endl;
+        cout << "2 " << front << " " << end << endl;
         for (uint32_t i = front ; i < end; ++i) {
-            cout << queue[i] << endl;
+            printf("%d, ", queue[i]);
         }
     }
-    else {
-        cout << "3" << endl;
+    else if (front > end) {
+        cout << "3 " << front << " " << end << endl;
         for (uint32_t i = front; i < capacity; ++i) {
-            cout << queue[i] << endl;
+            printf("%d, ", queue[i]);
         }
         for (uint32_t i = 0; i < end; ++i) {
-            cout << queue[i] << endl;
+            printf("%d, ", queue[i]);
         }
     }
     cout << "------------------------------------" << endl;
 }
-/*int main(void) {
+
+int main(void) {
 	Queue q;
-    ifstream input;
+    /*ifstream input;
     input.open("tinyGraph.txt");
     string line;
     if (input.is_open()) {
@@ -130,23 +152,23 @@ void Queue::printQueue() {
             }
         } while (line != "S");
         input.close();
+    }*/
+    //q.push(0);
+    //q.push(1);
+//    q.printQueue();
+
+    for (uint32_t i = 0; i < 100; i++) {
+        q.push(i);
     }
     q.printQueue();
-    q.clear();
-    input.open("tinyGraph.txt");
-    if (input.is_open()) {
-        do {
-            getline(input, line);
-            if (line != "S") {
-                uint32_t node, neighbour;
-                istringstream command(line);
-                command >> node;
-                command >> neighbour;
-                q.push(neighbour);
-            }
-        } while (line != "S");
-        input.close();
-    }
-    q.printQueue();
+    /*q.pop();
+    q.pop();
+    q.pop();
+    q.pop();
+    q.printQueue();*/
+//    for (uint32_t i = 100; i < 200; i++) {
+//        q.push(i);
+//    }
+//    q.printQueue();
 	return 0;
-}*/
+}
